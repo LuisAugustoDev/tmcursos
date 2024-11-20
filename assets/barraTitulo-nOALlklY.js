d65f6757e69d6466a8a28ed913a7c91840e9c082
LuisAugustoDev <fluisaugusto603@gmail.com>
2024-11-20 18:35:36 -0300

    Updates

---
+++ assets/barraTitulo-nOALlklY.js
@@ -0,0 +1,1 @@
+import{o as e,c as o,a,t as s}from"./index-CSDKmVUu.js";const r={class:"titulo"},l={__name:"barraTitulo",props:{texto:String},setup(t){return(c,n)=>(e(),o("div",r,[a("h1",null,s(t.texto),1)]))}};export{l as _};
---
+++ assets/logo-ext-roxo-BbGWQZ4e.js
@@ -0,0 +1,1 @@
+const o="/tmcursos/assets/logo-ext-roxo-Cjhr2Aa7.png";export{o as _};
